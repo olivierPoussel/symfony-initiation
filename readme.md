9240317b3e41fb06412f34d8146fb156c0dad75c
Olivier Poussel <olivier.poussel@gmail.com>
2021-01-04 18:14:49 +0100

    save du cours 04/01/2020

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Symfony Initiation
+
+Récupérer le projet
+```
+git clone https://github.com/olivierPoussel/symfony-initiation.git
+```
+
+Installer le projet
+```
+composer install
+```
+
+Lancer le projet
+```bash
+symfony server:start
+# ou
+composer require symfony/web-server-bundle
+php bin/console server:start
+# ou
+php -S localhost:8000 
+```
---
+++ src/Controller/HelloController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("helloName/{name}", name="helloName")
+     * 
+     * @param [string] $name
+     * 
+     * @return Response
+     */
+    public function helloName($name) : Response
+    {
+        return new Response($name);
+    }
+
+    /**
+     * @Route("helloList")
+     * 
+     * @return Response
+     */
+    public function helloList() : Response
+    {
+        return $this->render(
+            'hello.html.twig', 
+            ['title' => 'helloList']
+        );
+    }
+
+    /**
+     * @Route(
+     *  "helloParam/{param}",
+     *  requirements={"param"="\d+"}, 
+     *  methods={"GET"}
+     * )
+     * 
+     * @return Response
+     */
+    public function helloParamInt($param) : Response
+    {
+        $title = 'Param Integer';
+        $params = [$param];
+
+        return $this->render(
+            'base.html.twig', 
+            ['title' => $title, 'items' => $params]
+        );
+    }
+
+    /**
+     * @Route("helloParam/{param}", methods={"GET"})
+     * 
+     * @return Response
+     */
+    public function helloParamDefault($param) : Response
+    {
+        $title = 'Param Any';
+        $params = [$param];
+
+        return $this->render(
+            'base.html.twig', 
+            ['title' => $title, 'items' => $params]
+        );
+    }
+
+    /**
+     * @Route("helloRequest")
+     * 
+     * @return Response
+     */
+    public function helloRequest(Request $request) : Response
+    {
+        $title = 'resquest';
+        $params = $request->query->all();
+
+        return $this->render(
+            'base.html.twig', 
+            ['title' => $title, 'items' => $params]
+        );
+    }
+
+    /**
+     * @Route("helloTwig")
+     *
+     * @return Response
+     */
+    public function helloTwig() : Response
+    {
+        $title = "Je suis un titre";
+        $tab = ["string $title", 'string $title '];
+        $tab = ['string1' => "string $title", 'string2' => 'string $title', 'key3' => 154640156];
+
+        return $this->render('base.html.twig', ['title' => $title, 'items' => $tab]);
+    }
+
+    /**
+     * @Route("baseTwig")
+     *
+     * @return Response
+     */
+    public function baseTwig() : Response
+    {
+        return $this->render('base.html.twig');
+    }
+    /**
+     * @Route("/hello")
+     *
+     * @return Response
+     */
+    public function index() : Response
+    {
+        return new Response('hello');
+    }    
+}
